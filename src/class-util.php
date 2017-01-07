6c70939166f7892165bcda88173f5baff454e7b3
xlad <xlad@mail.com>
2017-01-07 02:03:03 +0200

    Create class-util.php

---
+++ src/class-util.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Utility php class.
+ *
+ * @since      0.0.0
+ *
+ * @package    Util
+ * @author     xlad <xlad@mail.com>
+ */
+abstract class Util {
+
+	/**
+	 * PHP - Convert mixed array & objects recursively.
+	 *
+	 * @since    0.0.0
+	 * @access   public
+	 * @var      array    $d    the mixed object/ array.
+	 * 
+	 * @initialAuthor    https://coderwall.com/joakley77
+	 * @source    https://coderwall.com/p/8mmicq/php-convert-mixed-array-objects-recursively
+	 */
+    public static function object_to_array($d) {
+        if (is_object($d))
+            $d = get_object_vars($d);
+
+        return is_array($d) ? array_map(__METHOD__, $d) : $d;
+    }
+
+    public static function array_to_object($d) {
+        return is_array($d) ? (object) array_map(__METHOD__, $d) : $d;
+    }
+    
+}
